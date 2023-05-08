b51bf7338c8b311a8c44419084e94c3e6a7fee17
veselin valkanov <veselin.d.valkanov@gmail.com>
2023-05-08 16:19:59 +0300

    added some new function MakeBidViewClient

--- Lottery/Lottery.sol
+++ Lottery/Lottery.sol
@@ -1,27 +1,45 @@
 // SPDX-License-Identifier:MIT
 
-pragma solidity 0.8.19;
+pragma solidity ^0.8.1;
 
 
 contract Lottery{
+
+    address owner;
+
     struct Client {
-        address id;
+        address clientid;
         uint256 amount;
     }
 
-    Client public
+   
+
     Client[] public clients;
     mapping(address => uint[]) public clintsAndTheirBids;
     mapping(address => bool) public IsClientExists;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "You aren't the owner");
+        _;
+    }
     
     modifier IsClient(address id) {
         require(!IsClientExists[id], "Cleint already exists");
         _;
     }
 
-    function AddClient(address id,uint256 bid) public IsClient(id) {
+    function AddClient(address clientid,uint256 bid) public IsClient(clientid) {
         require(bid >= 0.01 ether, "insufficient amount");
-        Client memory client = Client(id,bid);
-        clients.push(clinet);
+        Client memory client = Client(clientid,bid);
+        clients.push(client);
+    }
+
+    function MakeBid(address clientId, uint256 bid) public{
+        require(!IsClientExists[clientId], "Cleint already exists");
+        clintsAndTheirBids[clientId].push(bid);
     }
 }
